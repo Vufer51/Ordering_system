3075279fbacbc8ca13fbccf03fe40e356b9c08fd
Vufer51 <kpot.hopa@gmail.com>
2021-04-16 01:04:56 +0300

    Превью изображений для загрузки

--- components/infoForm.vue
+++ components/infoForm.vue
@@ -27,17 +27,47 @@
             <v-row>
               <v-col>
                 <v-text-field label="Модель"/>
+                <v-text-field label="Заводской №"/>
               </v-col>
               <v-col>
                 <v-combobox
                   label="Год выпуска"
                   :items="years"
                   :rules="[rules.notFuture]"
-                ></v-combobox>
-                {{dataWorks}}
+                />
+                <v-file-input
+                  @change="previewImages"
+                  v-model="images"
+                  multiple
+                  accept="image/png, image/jpeg, image/bmp"
+                  prepend-icon="mdi-camera"
+                  label="Фотографии"
+                >
+                </v-file-input>
               </v-col>
             </v-row>
 
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+            v-for="(image, n) in imageUrls"
+            :key="n"
+            class="d-flex child-flex"
+            cols="3"
+          >
+            <v-img
+              aspect-ratio="1"
+              :src="image"
+            >
+              <v-btn
+                class="mt-2" small
+                elevation="2"
+                @click="removeImage(n)"
+              >
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </v-img>
           </v-col>
         </v-row>
       </v-card-text>
@@ -48,9 +78,11 @@
 <script>
 export default {
   name: 'infoForm',
-  data() {
+  data () {
     return {
-      dataWorks:[],
+      images: null,
+      imageUrls: [],
+      dataWorks: [],
       currentYear: new Date().getFullYear(),
       techTypes: [
         'Кран',
@@ -60,7 +92,7 @@ export default {
       years: [],
       rules: {
         required: value => !!value || 'Поле обязательно.',
-        notEmptyArray: value => value.length>0 || 'Должны быть выбраны работы',
+        notEmptyArray: value => value.length > 0 || 'Должны быть выбраны работы',
         notFuture: value => value <= this.currentYear || 'Вы из будущего?',
         email: value => {
           const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -69,13 +101,23 @@ export default {
       },
     }
   },
-  created() {
+  created () {
 
     for (let x = this.currentYear; x > 1950; x--) {
       this.years.push(x)
     }
 
-
+  },
+  methods: {
+    removeImage(n) {
+      this.images.splice(n,1)
+      this.imageUrls.splice(n,1)
+    },
+    previewImages () {
+      this.images.forEach(image => {
+        this.imageUrls.push(URL.createObjectURL(image))
+      })
+    }
   }
 }
 </script>
